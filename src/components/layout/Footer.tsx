400b21553bbfd21286e0408e2531bc459c3771b8
ekishid <ekishidev@gmail.com>
2025-03-22 01:55:23 -0600

    Initial commit

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col gap-3 justify-center items-center py-3">
+      <ul className="flex flex-row gap-6">
+        <li><a href="#">LinkedIn</a></li>
+        <li><a href="#">GitHub</a></li>
+      </ul>
+      <p>ekishidev - Portfolio created with NextJs</p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex flex-row justify-between items-center px-5 py-5">
+      <a href="#">Logo</a>
+      <div className="cursor-pointer">Menu</div>
+    </header>
+  );
+};
+export default Header;
